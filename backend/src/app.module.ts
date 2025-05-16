805d0c1c1ea9083ca8c002c31de77e774ed3084b
JuanferGG <juanfe15777@gmail.com>
2025-05-16 09:49:08 -0500

    initial commit - main.ts basic config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: '.env',
-  }), UserModule,    
-
-],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(String(process.env.MONGODB_URI)),
+    UserModule,
+  ],
 })
 export class AppModule {}
