e8be162bb918a7bb095696784f427b3c1cb8a0b0
JuanferGG <juanfe15777@gmail.com>
2025-05-19 08:02:09 -0500

    initial commit frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+// TODO CSS
+import './App.css'
+
+// TODO Pages
+
+// TODO libraries
+
+function App() {
+  
+
+  return (
+    <section className='container w-full'>
+      <h1 className='text-2xl'>Hello Vite!</h1>
+    </section>
+  )
+}
+
+export default App
