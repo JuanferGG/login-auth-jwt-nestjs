32cef5c3f64e8cd63b35856b1b23d3bc75bbb1c8
JuanferGG <juanfe15777@gmail.com>
2025-07-03 17:18:36 -0500

    style(frontend): improve home page layout and edit user button styling

--- frontend/src/components/modals/Users/EditUser.tsx
+++ frontend/src/components/modals/Users/EditUser.tsx
@@ -155,7 +155,7 @@ export default function EditUserModal({
                   </button>
                   <button
                     type="submit"
-                    className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 cursor-pointer"
+                    className="bg-[#697565] text-white font-semibold px-4 py-2 rounded cursor-pointer"
                   >
                     Guardar Cambios
                   </button>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,8 +2,11 @@
 export default function HomePage() {
   
   return (
-    <div className="mt-6">
-       Home Page
+   <div className="mt-6 m-auto w-[90vw]">
+      <h1 className="font-bold text-[#222831] mb-4 text-4xl">Home Page</h1>
+      <div>
+        
+      </div>
     </div>
   )
 }
